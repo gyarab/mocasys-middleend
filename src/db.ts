524aca45533115cdd75b6cc98be0a450b489d658
Tomáš Černý <tmscer@gmail.com>
2019-02-02 10:53:05 +0100

    renamed a variable

--- src/db.ts
+++ src/db.ts
@@ -5,23 +5,23 @@ const pgConfig = config.get('db');
 export const pool = new pg.Pool(pgConfig);
 
 function makeQuery(query_str: string, data: any[], rowMode: boolean): pg.QueryConfig {
-    var q: pg.QueryConfig = {
+    var query: pg.QueryConfig = {
         text: query_str,
         values: data
     }
     if (rowMode) {
-        q['rowMode'] = 'array';
+        query['rowMode'] = 'array';
     }
-    return q;
+    return query;
 }
 
 export function query(query_str: string, data: any[], rowMode: boolean = true,
     callback: (err: Error, result: pg.QueryResult) => void): pg.Query {
-    var q = makeQuery(query_str, data, rowMode);
-    return pool.query(q, callback);
+    var query = makeQuery(query_str, data, rowMode);
+    return pool.query(query, callback);
 }
 
 export function queryPromise(query_str: string, data: any[], rowMode: boolean = true) {
-    var q = makeQuery(query_str, data, rowMode);
-    return pool.query(q);
+    var query = makeQuery(query_str, data, rowMode);
+    return pool.query(query);
 }
