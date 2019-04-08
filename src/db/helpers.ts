3d5fdb8191f2a6257de1bc25578e8907b7bbadf6
David Koňařík <dvd.kon@gmail.com>
2019-04-08 18:41:36 +0200

    Fix after BE table rename

--- src/db/helpers.ts
+++ src/db/helpers.ts
@@ -2,9 +2,9 @@ import * as db from './index';
 
 export function userPasswordHash(username: string) {
     return db.queryPromise(
-        'SELECT user_password_data.pw_hash, users_current.id, users_current.username ' +
-        'FROM user_password_data ' +
+        'SELECT user_passwords_data.pw_hash, users_current.id, users_current.username ' +
+        'FROM user_passwords_data ' +
         'INNER JOIN users_current ' +
-        'ON user_password_data.id_user = users_current.id ' +
+        'ON user_passwords_data.id_user = users_current.id ' +
         'WHERE users_current.username = $1;', [username]);
 }
