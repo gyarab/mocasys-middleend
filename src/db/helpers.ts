3cde78a51092f468cf459cf262f8915a41840d73
Tomáš Černý <tmscer@gmail.com>
2019-04-19 09:24:59 +0200

    /auth/reader finished

--- src/db/helpers.ts
+++ src/db/helpers.ts
@@ -1,6 +1,10 @@
 import * as db from './index';
 import * as pg from 'pg';
 
+function decodeFunctionCall(s: string, enc: string = 'hex') {
+    return `decode(${s}, '${enc}')`;
+}
+
 export function userPasswordHash(username: string): Promise<pg.QueryResult> {
     return db.queryPromise(
         'SELECT user_passwords_data.pw_hash, users_current.id, users_current.username ' +
@@ -13,10 +17,9 @@ export function userPasswordHash(username: string): Promise<pg.QueryResult> {
 
 export function cardIdAndSecretKey(card_id, secret_key): Promise<pg.QueryResult> {
     return db.queryPromise(
-        // TODO: Select data from food_choice_current - diners_current - users_current - user_mifare_cards_current
-        'SELECT * FROM user_mifare_cards_current ' +
-        'WHERE user_mifare_cards_current.card_id = $1 ' +
-        'AND user_mifare_cards_current.secret_key = $2;',
+        'SELECT user_mifare_cards_current.id_user FROM user_mifare_cards_current ' +
+        `WHERE user_mifare_cards_current.card_id = ${decodeFunctionCall('$1')} ` +
+        `AND user_mifare_cards_current.secret_key = ${decodeFunctionCall('$2')};`,
         [card_id, secret_key]
     );
 }
