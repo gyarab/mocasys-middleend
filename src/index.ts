83e06ff25c13daea87dd6d2df4fd77d7344ffd89
Tomáš Černý <tmscer@gmail.com>
2019-04-24 15:01:08 +0200

    /qdb - row description - tableName, dataTypeName and caching the desc on startup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as config from 'config';
 import * as corsMiddleware from 'restify-cors-middleware';
 import * as restify from 'restify';
+import * as renamer from './db/renamer';
 import { validateSessionToken } from './auth';
 import { masterRouter } from './endpoints';
 
@@ -27,13 +28,21 @@ server.use(validateSessionToken);
 
 if (serverConfig['logRequests']) {
     server.use((req, res, next) => {
-        //console.log(req.headers);
+        console.log(new Date(), req.params);
         return next();
     });
 }
 
 masterRouter.applyRoutes(server, serverConfig['rootPath']);
 
-server.listen(serverConfig['port'], 'localhost', () => {
-    console.log(`INFO: Node app is running at localhost:${serverConfig['port']}`);
-});
+function main() {
+    server.listen(serverConfig['port'], 'localhost', () => {
+        console.log(`INFO: Node app is running at localhost:${serverConfig['port']}`);
+    });
+}
+
+function init() {
+    renamer.init(main);
+}
+
+init();
--- src/middleResponse.ts
+++ src/middleResponse.ts
@@ -1,13 +1,45 @@
 import * as pg from 'pg';
+import * as renamer from './db/renamer';
+
+interface MField {
+    tableName: String;
+    // TODO: Replace with columnName
+    columnID: Number;
+    dataTypeName: String;
+    dataTypeSize: Number;
+    dataTypeModifier: Number;
+    format: String;
+}
+
+function transformField(field: pg.FieldDef): MField {
+    let newField = {
+        tableName: renamer.tableName(field.tableID),
+        columnID: field.columnID,
+        dataTypeName: renamer.typeName(field.dataTypeID),
+        dataTypeSize: field.dataTypeSize,
+        dataTypeModifier: field.dataTypeModifier,
+        format: field.format,
+    }
+    console.log(newField);
+    return newField;
+}
+
+function transformFields(fields: pg.FieldDef[]): Array<MField> {
+    let parsedFields = new Array<MField>(fields.length);
+    for (let i = 0; i < fields.length; i++) {
+        parsedFields[i] = transformField(fields[i]);
+    }
+    return parsedFields;
+}
 
 export class MiddleResponse {
     rows: any[][];
     rowCount: number;
-    fields: pg.FieldDef[];
+    fields: MField[];
 
     constructor(result: pg.QueryResult) {
         this.rows = result.rows;
         this.rowCount = result.rowCount;
-        this.fields = result.fields;
+        this.fields = transformFields(result.fields);
     }
 }
